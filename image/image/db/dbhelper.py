0a0c7469fa594986e8ba45db14fcb69aef4a16b2
Gao Qi(集团商业部) <qigao216538@sohu-inc.com>
2018-10-11 17:37:56 +0800

    feat: exception db保存异常处理

--- image/image/db/dbhelper.py
+++ image/image/db/dbhelper.py
@@ -9,7 +9,7 @@ class DBHelper():
     '''
     def __init__(self):
         
-        self.connect = pymysql.connect(
+        self.db = pymysql.connect(
             host=DB_CONFIG['MYSQL_HOST'],
             db=DB_CONFIG['MYSQL_DBNAME'],
             user=DB_CONFIG['MYSQL_USER'],
@@ -18,16 +18,24 @@ class DBHelper():
             port=DB_CONFIG['MYSQL_PORT']
         )
 
-        self.cursor = self.connect.cursor()
+        self.cursor = self.db.cursor()
 
     #插入数据库
     def insert(self, item):
-        cursor = self.cursor
-        sql = "insert into images(title,img_url) values(%s,%s)"
-        #调用插入的方法
-        cursor.execute(sql, (
-            item["title"], item['url']
+        try:
+            cursor = self.cursor
+            sql = "insert into images(title,img_url) values(%s,%s)"
+            #调用插入的方法
+            cursor.execute(sql, (
+                item["title"], item['url']
+                )
             )
-        )
-        self.connect.commit()
+            self.db.commit()
+
+        except Exception as e:
+            print('insert error', e)
+            self.db.rollback()
+        finally:
+            self.db.close()
+
         return item
