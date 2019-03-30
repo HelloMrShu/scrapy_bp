519b40416ba161a2532404c665d61b9276ac0f5d
gaoqi <gaoqi198@163.com>
2019-03-30 12:41:44 +0800

    feat: 调整路由及html文件路由

--- flask/app/main/views.py
+++ flask/app/main/views.py
@@ -2,12 +2,14 @@ from . import main
 from flask import render_template,request,url_for,redirect,abort,flash
 from app.models import Image,Article,db,Comment
 
-#首页
+
+# 首页
 @main.route('/')
 def index():
 	return 'Hello Flask !';
 
-#图片列表页面
+
+# 图片列表页面
 @main.route('/image/index', methods=['GET'])
 def image_index():
 	page = int(request.args.get('page') or 1)
@@ -17,7 +19,8 @@ def image_index():
 	image_list = paginate.items
 	return render_template('image.html', images=image_list, paginate=paginate)
 
-#文章创建,编辑保存
+
+# 文章创建,编辑保存
 @main.route('/article/create', methods=['GET', 'POST'])
 def article_create():
 	if request.method == "GET":
@@ -45,7 +48,8 @@ def article_create():
 		finally:
 			return redirect(url_for('main.article_index'))
 
-#文章保存
+
+# 文章保存
 @main.route('/article/index', methods=['GET'])
 def article_index():
 	page = int(request.args.get('page') or 1)
@@ -55,7 +59,8 @@ def article_index():
 	articles = paginate.items
 	return render_template('article/index.html', articles=articles, paginate=paginate)
 
-#编辑
+
+# 编辑
 @main.route('/article/edit', methods=['POST'])
 def article_edit():
 	article_id = request.form.get('article_id')
@@ -63,7 +68,8 @@ def article_edit():
 	article = Article.query.filter_by(id=article_id).first()
 	return render_template('/article/edit.html', article=article)
 
-#浏览
+
+# 浏览
 @main.route('/article/view', methods=['GET'])
 def article_view():
 	article_id = request.args.get('article_id')
@@ -73,6 +79,7 @@ def article_view():
 		return render_template('/article/view.html', article=article)
 	else:
 		abort(404)
+
 
 #文章评论
 @main.route('/article/comment', methods=['POST'])
