df72c9448bd3cbcd710ebd0551091ffd74eaad8f
gaoqi <gaoqi198@163.com>
2019-11-06 08:03:13 +0800

    feat: debug

--- flask/manage.py
+++ flask/manage.py
@@ -11,6 +11,8 @@ config_name = os.environ.get('FLASK_CONFIG') or 'default'
 
 # 生成app
 app = create_app(config_name)
+
+# 调试模式，模板代码修改立即生效. 等价于 TEMPLATES_AUTO_RELOAD = True
 app.config['DEBUG'] = True
 
 manager = Manager(app)
